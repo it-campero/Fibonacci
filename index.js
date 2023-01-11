2f992615cb06f0c1156f8d7596a1578cd8bf188d
Facundo Heinze <matiasrheinze@gmail.com>
2023-01-11 04:15:59 -0300

    Fobonacci con bucle + array

---
+++ index.js
@@ -0,0 +1,15 @@
+//Crea un nuevo fichero JS que contenga las siguientes líneas
+
+//- Una función que admita un parámetro "num", y devuelva una lista con esa cantidad de números de la secuencia de Fibonacci (Por ejemplo: num = 6 => Resultado [1, 1, 2, 3, 5, 8])
+
+let fibonacci = (limit) => {
+    let arrayFibo = [0,1];
+    for (let i = 2; i < limit; i++) {
+        arrayFibo[i] = arrayFibo[i-1]+ arrayFibo[i - 2]
+    };
+
+    return arrayFibo;
+};
+console.log(fibonacci(8));
+
+//- Ejecuta la depuración de VSCode para visualizar la ejecución de la función
